5e81c9d3780e341b42ed7816df9111f26bbd087e
root <root@DESKTOP-MEARCD8.localdomain>
2019-05-05 19:43:59 +0400

    change startup deploy and immutable.json

--- packer/files/deploy.sh
+++ packer/files/deploy.sh
@@ -2,9 +2,8 @@
 
 
 cat <<EOF > /etc/init.d/mystartup.sh
-#! /bin/bash
-su appuser -c 'source ~/.rvm/scripts/rvm'
-su appuser -c 'git clone https://github.com/Artemmkin/reddit.git'
-su appuser -c 'cd reddit && bundle install'
-su appuser -c 'puma -d'
+#! /bin/sh
+systemctl start mongod
+systemctl enable mongod
+su appuser -c 'source ~/.rvm/scripts/rvm && cd /home/appuser && git clone https://github.com/Artemmkin/reddit.git && cd reddit && bundle install && puma -d'
 EOF
