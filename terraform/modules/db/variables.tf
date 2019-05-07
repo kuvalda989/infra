8785e741c2a2690e66dfd5684a67b2b64ab15ac1
kuvalda989 <kuvalda989@mail.ru>
2019-05-07 19:12:45 +0400

    add modules

---
+++ terraform/modules/db/variables.tf
@@ -0,0 +1,8 @@
+variable private_key_path {
+  description = "Path to the public key used for ssh access"
+}
+
+variable db_disk_image {
+  description = "Disk image for reddit db"
+  default = "reddit-app-base"
+}
